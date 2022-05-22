881eeed60379e9648dc5104218fbc145a38a2469
Olive <olivelliott48@gmail.com>
2022-05-22 01:24:19 -0400

    Update search history function to persist data from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
-var searchButtonEl = $('#searchbtn');
+var searchButtonEl = $('#search-btn');
+var cities = [];
+
+function loadSearchHistory() {
+    // $('#search-history').each(function() {
+        var oldSearches = JSON.parse(localStorage.getItem('cities'));
+        citySearchEl = $('<button>').addClass('btn search-value').text(oldSearches).attr('type', 'submit');
+       $(citySearchEl).appendTo('#search-history');
+        console.log(oldSearches);
+    };
+
+
 
 function searchHistoryHandler() {
-    pastSearchValue = JSON.parse(localStorage.getItem("location"));
-    pastSearchElement = $('<button>').addClass('btn').text(pastSearchValue);
+    pastSearchValue = JSON.parse(localStorage.getItem('location'));
+    pastSearchElement = $('<button>').addClass('btn search-value').text(pastSearchValue);
     $(pastSearchElement).appendTo('#search-history');
 }
+
 
 $(searchButtonEl).on('click', function(event) {
     event.preventDefault();
     var searchValue = $(this).siblings('#search-item').val().trim();
-    localStorage.setItem('location', JSON.stringify(searchValue));
-    searchHistoryHandler();
+    cities.push(searchValue);
+    localStorage.setItem('cities', JSON.stringify(cities));
+    searchHistoryHandler;
 })
+
+loadSearchHistory();
