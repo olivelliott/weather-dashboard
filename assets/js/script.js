18c15e57dec0d2439490d2fc4450bd7ede02c7e7
Olive <olivelliott48@gmail.com>
2022-05-22 19:52:29 -0400

    Remove loadSearchHistory function for simplified code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,39 @@
 var searchButtonEl = $('#search-btn');
 var cities = [];
 
-function loadSearchHistory() {
-    // $('#search-history').each(function() {
-        var oldSearches = JSON.parse(localStorage.getItem('cities'));
-        citySearchEl = $('<button>').addClass('btn search-value').text(oldSearches).attr('type', 'submit');
-       $(citySearchEl).appendTo('#search-history');
-        console.log(oldSearches);
-    };
+//  ! TODO: unnecessary?
+// function loadSearchHistory() {
+//     // $('#search-history').each(function() {
+//         var oldSearches = JSON.parse(localStorage.getItem('cities'));
+//         for ( var i = 0; i < oldSearches.length; i++) {
+//         citySearchEl = $('<button>').addClass('btn search-value').text(oldSearches).attr('type', 'submit');
+//        $(citySearchEl).appendTo('#search-history');
+//         }
+//         console.log(oldSearches);
+//     };
 
 
 
 function searchHistoryHandler() {
-    pastSearchValue = JSON.parse(localStorage.getItem('location'));
+    var pastSearchValue = JSON.parse(localStorage.getItem('cities'));
+   for (var i = 0; i < pastSearchValue.length; i++) {
     pastSearchElement = $('<button>').addClass('btn search-value').text(pastSearchValue);
     $(pastSearchElement).appendTo('#search-history');
+   }; console.log(pastSearchValue)
 }
-
 
 $(searchButtonEl).on('click', function(event) {
     event.preventDefault();
     var searchValue = $(this).siblings('#search-item').val().trim();
+    //  ! TODO clear the input form after typing
     cities.push(searchValue);
     localStorage.setItem('cities', JSON.stringify(cities));
-    searchHistoryHandler;
+    searchHistoryHandler();
 })
 
-loadSearchHistory();
+searchHistoryHandler();
+
+// var string = "Hello World";
+// Now let's use the split() method and pass o in as the argument and separator, as shown here:
+
+// string.split("o");
