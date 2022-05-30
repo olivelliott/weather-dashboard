7e57bac8b4691e98ce4fab5f0777b92874fe5811
Olive <olivelliott48@gmail.com>
2022-05-29 23:39:21 -0400

    Update local storage function bug for repeated saves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var searchButtonEl = $('#search-btn');
 var weatherApi = '66d2d9bcf1100f15b471153e4495b6ac';
-// var currentDayEl = moment().format('dddd MMMM Do')
-var cities = [];
 var searchHistoryButton = $('.search-history-button');
+const weatherContainerEl = document.querySelector('.weather-container');
+var cities =  [];
 
+// JSON.parse(localStorage.getItem('usernames')) 
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -11,39 +12,46 @@ var searchHistoryButton = $('.search-history-button');
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city -->
 
+//  * I take the current location data and dynamically generate a button 
+// function searchButtonHandler(weatherData, currentLocation) {
+//     // var searchLocation = currentLocation
+//     // var array = cities.toString(' ');
+//     // $('<a>').attr('href', .appendTo('.search-history-button');
+//     localStorage.setItem('cities', JSON.stringify(cities));
+//     // loadSearchHistory();
+// }
 
-//  * When I click the search button, a button is dynamically generated and displayed in a card underneath
-//  ! To Do: Fix the multiple button issue
-// function cityButtonHandler() {
-//     var pastSearchValue = JSON.parse(localStorage.getItem('cities'));
-//    for (var i = 0; i < pastSearchValue.length; i++) {
-//     var searchHistoryButton = $('<button>').addClass('btn search-history-button').text(pastSearchValue[i]).attr('type', 'submit');
-//     console.log(searchHistoryButton);
-//     $(searchHistoryButton).appendTo('#search-history');
-//    }; fetchWeatherApi();
+// function loadSearchHistory() {
+//     var searchHistory = JSON.parse(localStorage.getItem('cities'))
+//     console.log(searchHistory);
 // }
 
 
-//  * When I click the submit button, the search is pushed to an array and stored in localStorage
-// * API function is then called
+
+//  * When I click the submit button, I dynamically generate a button that will be displayed as search history
+//  * I then push that value to save it into localStorage
 $(searchButtonEl).on('click', function(event) {
     event.preventDefault();
     var searchValue = $(this).siblings('#search-item').val().trim();
     if (searchValue) {
-        // fetchWeatherApi(searchValue);
-        placeIdCoordinates(searchValue);
-        cities.push(searchValue);
-        localStorage.setItem('cities', JSON.stringify(cities));
+        searchButton(searchValue);
         $('#search-item').val('');
     } else {
         alert('Please enter a city name');
-    }
+    } 
     // searchHistoryHandler();
     // cityButtonHandler();
 });
 
+function searchButton(searchValue) {
+    var searchButtonHolder = $('<ul>').addClass('search-history col').appendTo('#search-history');
+    $('<li>' + searchValue + '</li>').addClass('btn search-history-button').appendTo(searchButtonHolder);
+    placeIdCoordinates(searchValue);
+}
+
 // *  I fetch the api data for exchanging search for lat/lon
 function placeIdCoordinates (searchValue) {
+    // $('#current-forecast').innerHTML = '';
     var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchValue + '&appid=' + weatherApi;
     fetch(requestUrl).then(function (response) {
         if (response.ok) {
@@ -51,6 +59,8 @@ function placeIdCoordinates (searchValue) {
                 var currentLocation = coordinateData[0].name;
                 var currentLon = coordinateData[0].lon;
                 var currentLat = coordinateData[0].lat;
+                cities.push(currentLocation);
+                localStorage.setItem('cities', JSON.stringify(cities));
                 fetchWeatherApi(currentLon, currentLat, currentLocation);
             })
         }
@@ -73,8 +83,6 @@ function fetchWeatherApi(currentLon, currentLat, currentLocation) {
     //    ! To do: create a catch
   };
 
-//   $('#current-forecast').style.display = 'block';
-
 // DONE: the temperature, the humidity, the wind speed, and the UV index, an icon representation of weather conditions, current location
     //  * I take the data from the Api and dynamically place it into the DOM to display current forecast
 function displayForecast(weatherData, currentLocation) {
@@ -88,7 +96,6 @@ function displayForecast(weatherData, currentLocation) {
         var currentHumidityEl = 'Current Humidity : ' + weatherData.current.humidity + '%';
         var currentWindSpeedEl = 'Current Wind Speed : ' + weatherData.current.wind_speed + ' MPH';
         var currentUVIndexEl = weatherData.current.uvi;
-
         var iconCode = weatherData.current.weather[0].icon;
         var iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png';
         $('#web-icon').attr('src', iconUrl);
@@ -107,25 +114,27 @@ function displayForecast(weatherData, currentLocation) {
         }
     } fiveDayForecast(weatherData, currentLocation);
 };
-// //  * I take the data from the Api and dynamically create 5 day forecast data
-function fiveDayForecast(weatherData, currentLocation) {
-    console.log(currentLocation);
+
+
+// * I take the data from the Api and dynamically create 5 day forecast data
+function fiveDayForecast(weatherData) {
     var fiveDayData = weatherData.daily;
     var cardHolder = $('<div>').addClass('forecast-info row').appendTo('#forecast-cards');
 
     for (var i=0; i < 5; i++) {
         var forecastCardEl = $('<div>').addClass('card').appendTo(cardHolder);
         $('<h2>').addClass('card-header').text(moment().add([i], 'day')).appendTo(forecastCardEl);
-        var forecastTemp = 'High : ' + fiveDayData[i].temp.day + ' F';
+        var forecastHighTemp = 'High : ' + fiveDayData[i].temp.day + ' F';
+        var forecastLowTemp = 'Low : ' + fiveDayData[i].temp.night + 'F';
         var forecastHumidity = 'Humidity : ' + fiveDayData[i].humidity + '%';
         var forecastWindSpeed = 'Wind Speed : ' + fiveDayData[i].wind_speed + ' MPH';
         var forecastUVIndex = fiveDayData[i].uvi;
-
         var iconCode = fiveDayData[i].weather[0].icon;
         var iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png';
-        $('<img>').attr('src', iconUrl).appendTo(forecastCardEl);
 
-        $('<li>' + forecastTemp + '</li>').appendTo(forecastCardEl);
+        $('<img>').attr('src', iconUrl).appendTo(forecastCardEl);
+        $('<li>' + forecastHighTemp + '</li>').appendTo(forecastCardEl);
+        $('<li>' + forecastLowTemp + '</li>').appendTo(forecastCardEl);
         $('<li>' + forecastHumidity + '</li>').appendTo(forecastCardEl);
         $('<li>' + forecastWindSpeed + '</li>').appendTo(forecastCardEl);
 
@@ -136,50 +145,67 @@ function fiveDayForecast(weatherData, currentLocation) {
         } else {
             $('<li>' + 'UV Index : ' + forecastUVIndex + '</li>').addClass('index badIndex').appendTo(forecastCardEl);
         }
-    } searchButtonHandler(weatherData, currentLocation);
+    } 
+    // searchButtonHandler(weatherData, currentLocation);
 }
 
-function searchButtonHandler(weatherData, currentLocation) {
-    $('<button>' + currentLocation + '</button>').addClass('btn search-history-button').attr('type', 'submit').appendTo('#search-history');
+function loadSearchHistory() {
+    var searchHistoryEl = JSON.parse(localStorage.getItem('cities'));
+    for (var i = 0; i < searchHistoryEl.length; i++) {
+        searchValue = searchHistoryEl[i]
+        searchButton(searchValue);
+    }
 }
 
+loadSearchHistory();
+
+
+// $('#search-btn').on('click', searchButtonHandler);
+// loadSearchHistory();
+
+
+//  * When I click the search button, a button is dynamically generated and displayed in a card underneath
+//  ! To Do: Fix the multiple button issue
+// function cityButtonHandler() {
+//     var pastSearchValue = JSON.parse(localStorage.getItem('cities'));
+//    for (var i = 0; i < pastSearchValue.length; i++) {
+//     var searchHistoryButton = $('<button>').addClass('btn search-history-button').text(pastSearchValue[i]).attr('type', 'submit');
+//     console.log(searchHistoryButton);
+//     $(searchHistoryButton).appendTo('#search-history');
+//    }; fetchWeatherApi();
+// }
+
+
+// $('.search-history-button').on('submit', displayForecast());
 
 
 
-// 0:
-// clouds: 100
-// dew_point: 58.96
-// dt: 1653498000
-// feels_like: {day: 68.92, night: 63.34, eve: 65.52, morn: 59.54}
-// humidity: 71
-// moon_phase: 0.85
-// moonrise: 1653464040
-// moonset: 1653508440
-// pop: 0.08
-// pressure: 1024
-// sunrise: 1653472975
-// sunset: 1653524425
-// temp:
-    // day: 68.99
-    // eve: 65.43
-    // max: 70.68
-    // min: 58.82
-    // morn: 59.68
-    // night: 63.14
-// [[Prototype]]: Object
-// uvi: 6.94
-// weather: Array(1)
-// 0:
-// description: "overcast clouds"
-// icon: "04d"
-// id: 804
-// main: "Clouds"
-// [[Prototype]]: Object
-// length: 1
-// [[Prototype]]: Array(0)
-// wind_deg: 51
-// wind_gust: 17.07
-// wind_speed: 9.37
+// function saveSearchHistory(searchValue) {
+//     if (cities) {
+//         cities.push(searchValue);
+//         localStorage.setItem('cities', JSON.stringify(cities));
+//         placeIdCoordinates(searchValue);
+//     } else if (!cities) {
+//         cities.push(searchValue);
+//         localStorage.setItem('cities', JSON.stringify(cities));
+//         placeIdCoordinates(searchValue);
+//     }
+// }
+
+// //  * I find the value of the search and push it to dynamically generate a button for that location
+// function searchButtonHandler(searchValue) {
+//     var searchValue = $(this).siblings('#search-item').val().trim();
+//     if (searchValue) {
+//         placeIdCoordinates(searchValue);
+//     } else {
+//     } 
+// }
+
+// function searchHistoryBtn(searchValue) {
+//     var searchButtonHolder = $('<ul>').addClass('search-history col').appendTo('#search-history');
+//     $('<li>' + searchValue + '</li>').addClass('btn search-history-button').appendTo(searchButtonHolder);
+//     placeIdCoordinates(searchValue);
+// }
 
 
 
@@ -192,12 +218,6 @@ function searchButtonHandler(weatherData, currentLocation) {
 
 
 
-
-
-// var string = 'Hello World';
-// Now let's use the split() method and pass o in as the argument and separator, as shown here:
-
-// string.split('o');
 
 // function loadSearchHistory() {
 //     // $('#search-history').each(function() {
